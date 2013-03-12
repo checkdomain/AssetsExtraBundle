f4b9e7650a1977e53e5a9348f70a40f920629578
Florian Körner <contact@florian-koerner.eu>
2013-03-12 10:33:47 +0100

    E-Mail-Adressen korrigiert.

--- CheckdomainAssetsExtraBundle.php
+++ CheckdomainAssetsExtraBundle.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * (c) Florian Koerner <contact@florian-koerner.eu>
+ * (c) Florian Koerner <f.koerner@checkdomain.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
