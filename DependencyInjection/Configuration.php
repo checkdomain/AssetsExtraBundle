6873e49893bd449c249075018d7b9c21651e8dce
Florian Koerner <contact@florian-koerner.eu>
2013-04-12 12:58:23 +0300

    fixed #4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,7 +25,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('assets_extra');
+        $rootNode = $treeBuilder->root('checkdomain_assets_extra');
 
         $rootNode->children()
                  ->booleanNode('encrypt_bundle')->defaultFalse()->end()
